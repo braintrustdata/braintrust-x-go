48160acfaa380830e437243e06af387129d79dd6
Matt Perpick <matt@braintrustdata.com>
2025-04-18 13:33:20 -0400

    WIP

--- traceopenai/traceopenai.go
+++ traceopenai/traceopenai.go
@@ -1,3 +1,37 @@
 package traceopenai
 
-const TEST = "123"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type NextMiddleware = func(req *http.Request) (*http.Response, error)
+
+func Middleware(req *http.Request, next NextMiddleware) (*http.Response, error) {
+	if req.Body != nil {
+		bodyBytes, err := io.ReadAll(req.Body)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Printf("Request Body: %s\n", string(bodyBytes))
+		req.Body = io.NopCloser(bytes.NewReader(bodyBytes))
+	}
+
+	resp, err := next(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Body != nil {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Printf("Response Body: %s\n", string(bodyBytes))
+		resp.Body = io.NopCloser(bytes.NewReader(bodyBytes))
+	}
+
+	return resp, nil
+}
--- traceopenai/traceopenai_test.go
+++ traceopenai/traceopenai_test.go
@@ -4,47 +4,73 @@ import (
 	"context"
 	"testing"
 
+	"github.com/openai/openai-go"
+	"github.com/openai/openai-go/option"
+	"github.com/openai/openai-go/responses"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 )
 
-func setUp() (*tracetest.InMemoryExporter, func()) {
+const TEST_MODEL = openai.ChatModelGPT4oMini
+
+func setUpTracedClient() (openai.Client, *tracetest.InMemoryExporter, func()) {
+	// setup otel
 	exporter := tracetest.NewInMemoryExporter()
 	tp := trace.NewTracerProvider(
 		trace.WithSyncer(exporter), // flushes immediately
 	)
 	otel.SetTracerProvider(tp)
-
 	teardown := func() {
 		otel.SetTracerProvider(nil)
 	}
+	// set up traced openai client
+	client := openai.NewClient(
+		option.WithMiddleware(Middleware),
+	)
 
-	return exporter, teardown
+	return client, exporter, teardown
+}
+
+func TestOpenAIResponses(t *testing.T) {
+	client, exporter, teardown := setUpTracedClient()
+	defer teardown()
+	assert := assert.New(t)
+
+	params := responses.ResponseNewParams{
+		Input: responses.ResponseNewParamsInputUnion{OfString: openai.String("Hello, world!")},
+		Model: TEST_MODEL,
+	}
+
+	resp, err := client.Responses.New(context.Background(), params)
+	assert.NoError(err)
+	assert.NotNil(resp)
+
+	spans := flushSpans(exporter)
+	assert.NotEmpty(spans)
 }
 
 func TestOTelSetup(t *testing.T) {
-	exporter, teardown := setUp()
+	_, exporter, teardown := setUpTracedClient()
 	defer teardown()
-
 	assert := assert.New(t)
 
-	spans := flush(exporter)
+	// crudely check we can create and test spans
+	spans := flushSpans(exporter)
 	assert.Empty(spans)
 
 	tracer := otel.Tracer("test")
 	_, span := tracer.Start(context.Background(), "test")
 	span.End()
 
-	spans = flush(exporter)
+	spans = flushSpans(exporter)
 	assert.NotEmpty(spans)
-
-	spans = flush(exporter)
+	spans = flushSpans(exporter)
 	assert.Empty(spans)
 }
 
-func flush(exporter *tracetest.InMemoryExporter) []tracetest.SpanStub {
+func flushSpans(exporter *tracetest.InMemoryExporter) []tracetest.SpanStub {
 	spans := exporter.GetSpans()
 	exporter.Reset()
 	return spans
