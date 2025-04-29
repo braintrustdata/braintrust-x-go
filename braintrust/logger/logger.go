19017f064b2f28a919b9598739c64ac5c778d88c
Matt Perpick <matt@braintrustdata.com>
2025-04-29 18:12:06 -0400

    remving old code

--- braintrust/logger/logger.go
+++ braintrust/logger/logger.go
@@ -1,8 +1,6 @@
 package logger
 
 import (
-	"bytes"
-	"io"
 	"log"
 	"sync"
 )
@@ -60,48 +58,4 @@ func (stdLogger) Debugf(format string, args ...any) {
 
 func (stdLogger) Warnf(format string, args ...any) {
 	log.Printf("WARN braintrust: "+format, args...)
-}
-
-// bufferedReader saves data read from the readCloser and triggers an action
-// when fully read or closed.
-type bufferedReader struct {
-	src    io.ReadCloser
-	buf    *bytes.Buffer
-	onDone func(io.Reader) // called once when fully read or closed
-	once   sync.Once
-	closed bool
-}
-
-func newBufferedReader(src io.ReadCloser, onDone func(io.Reader)) *bufferedReader {
-	return &bufferedReader{
-		src:    src,
-		buf:    &bytes.Buffer{},
-		onDone: onDone,
-	}
-}
-
-func (r *bufferedReader) Read(p []byte) (int, error) {
-	n, err := r.src.Read(p)
-	if n > 0 {
-		_, _ = r.buf.Write(p[:n])
-	}
-	if err == io.EOF {
-		r.trigger()
-	}
-	return n, err
-}
-
-func (r *bufferedReader) Close() error {
-	r.closed = true
-	r.trigger()
-	return r.src.Close()
-}
-
-// trigger ensures onDone is only called once
-func (r *bufferedReader) trigger() {
-	r.once.Do(func() {
-		if r.onDone != nil {
-			r.onDone(r.buf)
-		}
-	})
 }
